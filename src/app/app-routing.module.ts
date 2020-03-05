6049f3fb5a5d862cc3e1a1fd3132bde67190ecc9
maissa <maissaboughzella@gmail.com>
2020-03-05 13:09:18 +0100

    backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent },
+  {path: 'account', component: AccountComponent },
 ];
 
 @NgModule({
