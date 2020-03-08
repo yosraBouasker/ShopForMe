2cbb2e76e9ada0c6b1f094d845f5a59560f60a8e
maissa <maissaboughzella@gmail.com>
2020-03-08 13:42:50 +0100

    verification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AccountComponent } from './account/account.component';
-import { CarouselComponent } from './carousel/carousel.component';
+import { ShopGridComponent } from './shop-grid/shop-grid.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent },
-  {path: 'car', component: CarouselComponent },
-  {path: 'account', component: AccountComponent },
+  {path: '', component: HomeComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent },
+  {path: 'shop', component: ShopGridComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AccountComponent } from './account/account.component';
-import { CarouselComponent } from './carousel/carousel.component';
+import { ShopGridComponent } from './shop-grid/shop-grid.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigatorComponent } from './navigator/navigator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AccountComponent,
-    CarouselComponent
+    ShopGridComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
