b957fb36b6e654737d0d1c9bc54b4356e3e0ac87
yosraBouasker <yosrabouasker5@gmail.com>
2020-03-11 20:36:46 +0100

    crud category

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
 const user = require('../models/category');
 module.exports = router;
+
+router.post('/addCategory', async (req, res) => {
+  const CategoryResult = await category.create(req.body).catch(err => err);
+  res.send({ data: CategoryResult })
+})
