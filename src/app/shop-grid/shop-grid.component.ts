a4783d2b56fe00df70b70ab8ac36f10abbcf1f6d
yosraBouasker <yosrabouasker5@gmail.com>
2020-03-20 20:44:16 +0100

    shop grid

--- src/app/shop-grid/shop-grid.component.ts
+++ src/app/shop-grid/shop-grid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiCategoryService } from '../shared/api-category.service';
+import { ApiProductService } from '../shared/api-product.service';
 
 @Component({
   selector: 'app-shop-grid',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopGridComponent implements OnInit {
 
-  constructor() { }
+  categories = [];
+  subCategories = [];
+  products=[];
+  constructor(private apiCategoryService: ApiCategoryService, private apiProductService: ApiProductService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    //console.log('here')
+    this.apiCategoryService.getCategories().subscribe((res: any) => {
+      //console.log(res);
+      this.categories = res.data;
+      this.subCategories = res.data[0].subCategories;
+    });
+
+    this.apiProductService.getProducts().subscribe((res: any) => {
+      console.log(res);
+      this.products = res.data;
+    });
+  }
+
+  getFinalPrice(price, discount, id, name) : number{
+    
+    var dis = document.getElementById(name);
+    var off = document.getElementById(id);
+
+    if (discount == 0){
+      dis.style.visibility = "hidden";
+      off.style.visibility = "hidden";
+      return price;
+    }
+    else {
+      var initialPrice= price - (price*discount)/100;
+      return initialPrice;
+    }
   }
 
 }
